5d391b37afa09ce66ebce997f39c6a83b3d0a42e
Timothée Burgmeier <burgmeiertim@gmail.com>
2025-12-03 10:16:15 +0100

    Created WIP branch

--- dispenser_lib/dispenser.hpp
+++ dispenser_lib/dispenser.hpp
@@ -1,9 +1,38 @@
 /*********************************************************************
- * @file  Apllication.cpp
- * @author <mettre l'adresse mail ou nom prenom>
- * @brief Fichier source de l'application
+ * @file  dispenser.hpp
+ * @author Burgmeier Timothée & Louis-le-Denmat Raphaël
+ * @brief Dispenser header file
  *********************************************************************/
 #ifndef DISPENSER_HPP
 #define DISPENSER_HPP
+
+/**
+ * @class food_dispenser
+ * @brief food_dispenser Class
+ */
+class food_dispenser
+{
+public:
+    /**
+     * @fn food_dispenser();
+     * @brief Default constructor
+     */
+    food_dispenser();
+    /**
+     * @fn food_dispenser();
+     * @brief Destructor
+     */
+    ~food_dispenser();
+    /**
+     * @fn void init(void)
+     * @brief Initialization function of the food dispenser
+     */
+    void init(void);
+    /**
+     * @fn void run(void)
+     * @brief Run function of the food dispenser
+     */
+    void run(void);
+};
 
 #endif // DISPENSER_HPP

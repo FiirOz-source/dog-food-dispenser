5a8c039fee69d3e5159a004bcbd859c348b1e448
Timothée Burgmeier <burgmeiertim@gmail.com>
2025-12-11 12:59:44 +0100

    Updated to make two leds blink

--- dog-food-dispenser.ino
+++ dog-food-dispenser.ino
@@ -1,16 +1,20 @@
 #include "dispenser_lib/dispenser.hpp"
 
 #define LED_PIN 2
+#define LED_PIN2 12
 
 void setup()
 {
     pinMode(LED_PIN, OUTPUT);
+    pinMode(LED_PIN2, OUTPUT);
 }
 
 void loop()
 {
     digitalWrite(LED_PIN, HIGH);
+    digitalWrite(LED_PIN2, HIGH);
     delay(500);
     digitalWrite(LED_PIN, LOW);
+    digitalWrite(LED_PIN2, LOW);
     delay(500);
 }

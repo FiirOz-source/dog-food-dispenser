70f16fbfa94a86ad8cad6461f42dd94aa16eb898
Timothée Burgmeier <burgmeiertim@gmail.com>
2025-12-11 14:28:37 +0100

    Test RFID reader

--- dog-food-dispenser.ino
+++ dog-food-dispenser.ino
@@ -1,20 +1,33 @@
 #include "dispenser_lib/dispenser.hpp"
+#include <SoftwareSerial.h>
 
 #define LED_PIN 2
 #define LED_PIN2 12
+
+SoftwareSerial rfid(13, -1);
 
 void setup()
 {
     pinMode(LED_PIN, OUTPUT);
     pinMode(LED_PIN2, OUTPUT);
+    Serial.begin(115200);
+    Serial.println("\nRFID prêt (lecture via Serial RX0 / GPIO3)");
+    rfid.begin(9600);
 }
 
 void loop()
 {
-    digitalWrite(LED_PIN, HIGH);
-    digitalWrite(LED_PIN2, HIGH);
-    delay(500);
-    digitalWrite(LED_PIN, LOW);
-    digitalWrite(LED_PIN2, LOW);
-    delay(500);
+    static int i = 0;
+    if (i == 0)
+    {
+        Serial.print("Test loop : ");
+        i++;
+    }
+
+    if (rfid.available())
+    {
+        byte c = rfid.read();
+        Serial.print(c);
+        Serial.print(" ");
+    }
 }

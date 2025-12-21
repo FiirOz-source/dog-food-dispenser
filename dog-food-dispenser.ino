b0c289f0fbb1756bc8f3eb0ef44f5df8aff6e815
Timothée Burgmeier <burgmeiertim@gmail.com>
2025-12-21 17:39:34 +0100

    Updated test of LCD screen and Servo motor

--- dog-food-dispenser.ino
+++ dog-food-dispenser.ino
@@ -1,9 +1,9 @@
 #include <dispenser.hpp>
 #include <actuators.hpp>
 
-static const uint8_t SDA_PIN = 4; // D2 sur NodeMCU
-static const uint8_t SCL_PIN = 5; // D1 sur NodeMCU
-static const uint8_t SERVO_PIN = 14;
+static const uint8_t SDA_PIN = 4;
+static const uint8_t SCL_PIN = 5;
+static const uint8_t SERVO_PIN = 14; // D5 on NODEMCU
 
 // food_dispenser dispenser;
 dispenser_lib::actuators::lcd_screen lcd_screen(SDA_PIN, SCL_PIN);
@@ -13,20 +13,38 @@ void setup()
 {
     // dispenser.init();
     lcd_screen.init_actuator();
+    static int init = 0;
+
+    char buf[32];
+    snprintf(buf, sizeof(buf), "Init %d%%", init);
+    lcd_screen.display_message(buf, 1, 0);
+    init += 10;
+    delay(500);
+
     servo_motor.init_actuator();
+
+    snprintf(buf, sizeof(buf), "Init %d%%", init);
+    lcd_screen.display_message(buf, 1, 0);
+    init += 10;
+    delay(500);
+
+    servo_motor.toggle_position();
+    delay(300);
+    servo_motor.toggle_position();
+
+    snprintf(buf, sizeof(buf), "Init %d%%", init);
+    lcd_screen.display_message(buf, 1, 0);
+    init += 10;
+    delay(500);
 
     lcd_screen.display_message("Dog Feeder", 0, 0);
 
-    static int init = 0;
-    for (int i = 0; i < 10; i++)
+    for (init; init < 100; init += 10)
     {
-        char buf[32];
         snprintf(buf, sizeof(buf), "Init %d%%", init);
         lcd_screen.display_message(buf, 1, 0);
         delay(500);
-        init += 10;
     }
-    char buf[32];
     snprintf(buf, sizeof(buf), "Init %d%%", init);
     lcd_screen.display_message(buf, 1, 0);
     delay(500);
@@ -36,6 +54,4 @@ void setup()
 void loop()
 {
     // dispenser.run();
-    servo_motor.toggle_position();
-    delay(1000);
 }

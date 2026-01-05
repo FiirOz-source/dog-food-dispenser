99b07ae56718f68151b7065f4cb528c4fceaf885
Timothée Burgmeier <burgmeiertim@gmail.com>
2026-01-05 15:57:50 +0100

    Added Web Page + Using interruption to handle feeding

--- dog-food-dispenser.ino
+++ dog-food-dispenser.ino
@@ -2,6 +2,7 @@
 #include <actuators.hpp>
 #include <sensors.hpp>
 #include <dogs.hpp>
+#include <Arduino.h>
 
 static const uint8_t SDA_PIN = 4;
 static const uint8_t SCL_PIN = 5;
@@ -18,8 +19,108 @@ dispenser_lib::actuators::servo_motor servo_motor(SERVO_PIN);
 dispenser_lib::sensors::ultrasonic_sensor ultrasonic_sensor(ULTRASONIC_SENSOR_PIN);
 dispenser_lib::sensors::infrared_sensor infrared_sensor(IR_PIN);
 dispenser_lib::sensors::rfid_sensor rfid_sensor(RFID_RX_PIN, -1, 9600); // RX, TX, Baudrate
+
+volatile bool ir_event = false;
+volatile uint32_t last_isr_us = 0;
+
+static const uint32_t IR_DEBOUNCE_US = 200000; // 200 ms
+
+void ICACHE_RAM_ATTR on_IR_falling()
+{
+    uint32_t now = micros();
+    if (now - last_isr_us < IR_DEBOUNCE_US)
+        return;
+    last_isr_us = now;
+    ir_event = true;
+}
+
+void show_waiting_screen()
+{
+    lcd_screen.clear();
+    lcd_screen.display_message("Dog Feeder", 0, 0);
+    lcd_screen.display_message("Waiting for dog", 1, 0);
+}
+
+void handleDogDetected()
+{
+    long distance = ultrasonic_sensor.get_distance();
+
+    if (distance < 100)
+    {
+        String rfid_tag = rfid_sensor.read_rfid(100);
+
+        bool jop_matched = Jop.matches_tag(rfid_tag);
+        bool manouk_matched = Manouk.matches_tag(rfid_tag);
+
+        if (jop_matched || manouk_matched)
+        {
+            if (jop_matched)
+            {
+                if (Jop.can_feed())
+                {
+                    Jop.mark_fed();
+                    lcd_screen.clear();
+                    lcd_screen.display_message("Feeding Jop!", 0, 0);
+                    servo_motor.open();
+                    delay(2000);
+                    servo_motor.close();
+                }
+                else
+                {
+                    lcd_screen.clear();
+                    lcd_screen.display_message("Jop already fed", 0, 0);
+                    String since = Jop.since_fed();
+                    lcd_screen.display_message(since.c_str(), 1, 0);
+                    delay(2000);
+                }
+            }
+            else
+            {
+                if (Manouk.can_feed())
+                {
+                    Manouk.mark_fed();
+                    lcd_screen.clear();
+                    lcd_screen.display_message("Feeding Manouk!", 0, 0);
+                    servo_motor.open();
+                    delay(2000);
+                    servo_motor.close();
+                }
+                else
+                {
+                    lcd_screen.clear();
+                    lcd_screen.display_message("Manouk alrdy fed", 0, 0);
+                    String since = Manouk.since_fed();
+                    lcd_screen.display_message(since.c_str(), 1, 0);
+                    delay(2000);
+                }
+            }
+        }
+        else
+        {
+            if (rfid_tag != "")
+            {
+                lcd_screen.clear();
+                lcd_screen.display_message("Unknown dog...", 0, 0);
+                char buf[32];
+                snprintf(buf, sizeof(buf), "Tag: %s", rfid_tag.c_str());
+                lcd_screen.display_message(buf, 1, 0);
+                delay(2000);
+            }
+        }
+    }
+    else
+    {
+        lcd_screen.clear();
+        lcd_screen.display_message("No more food ...", 0, 0);
+        delay(500);
+    }
+}
+
 void setup()
 {
+    Serial.begin(115200);
+    delay(100);
+
     lcd_screen.init_actuator();
     lcd_screen.display_message("Dog Feeder", 0, 0);
     static int init = 0;
@@ -61,95 +162,37 @@ void setup()
     lcd_screen.display_message(buf, 1, 0);
     delay(500);
     lcd_screen.display_message("Init Complete", 1, 0);
+
+    pinMode(IR_PIN, INPUT_PULLUP);
+    attachInterrupt(digitalPinToInterrupt(IR_PIN), on_IR_falling, FALLING);
+    delay(500);
+    show_waiting_screen();
 }
 
 void loop()
 {
-    static int previous_state = LOW;
-    static bool waiting_msg = true;
-    int current_state = infrared_sensor.get_state();
-    if ((current_state == LOW) && (previous_state == HIGH))
+    static bool busy = false;
+    static bool waiting_msg = false;
+
+    if (!waiting_msg && !busy)
     {
+        show_waiting_screen();
         waiting_msg = true;
-        long distance = ultrasonic_sensor.get_distance();
-        if (distance < (long)100)
-        {
-            String rfid_tag = rfid_sensor.read_rfid(100);
-            bool jop_matched = Jop.matches_tag(rfid_tag);
-            bool manouk_matched = Manouk.matches_tag(rfid_tag);
-            if ((jop_matched) || (manouk_matched))
-            {
-                if (jop_matched)
-                {
-                    if (Jop.can_feed())
-                    {
-                        Jop.mark_fed();
-                        lcd_screen.clear();
-                        lcd_screen.display_message("Feeding Jop!", 0, 0);
-                        servo_motor.open();
-                        delay(2000);
-                        servo_motor.close();
-                    }
-                    else
-                    {
-                        lcd_screen.clear();
-                        lcd_screen.display_message("Jop already fed", 0, 0);
-                        String since = Jop.since_fed();
-                        lcd_screen.display_message(since.c_str(), 1, 0);
-                        delay(2000);
-                    }
-                }
-                else if (manouk_matched)
-                {
-                    if (Manouk.can_feed())
-                    {
-                        Manouk.mark_fed();
-                        lcd_screen.clear();
-                        lcd_screen.display_message("Feeding Manouk!", 0, 0);
-                        servo_motor.open();
-                        delay(2000);
-                        servo_motor.close();
-                    }
-                    else
-                    {
-                        lcd_screen.clear();
-                        lcd_screen.display_message("Manouk alrdy fed", 0, 0);
-                        String since = Manouk.since_fed();
-                        lcd_screen.display_message(since.c_str(), 1, 0);
-                        delay(2000);
-                    }
-                }
-            }
-            else
-            {
-                if (rfid_tag != "")
-                {
-                    lcd_screen.clear();
-                    lcd_screen.display_message("Unknown dog...", 0, 0);
-                    char buf[32];
-                    snprintf(buf, sizeof(buf), "Tag: %s", rfid_tag.c_str());
-                    lcd_screen.display_message(buf, 1, 0);
-                    previous_state = current_state;
-                    delay(2000);
-                }
-            }
-        }
-        else
-        {
-            lcd_screen.clear();
-            lcd_screen.display_message("No more food ...", 0, 0);
-            delay(500);
-        }
     }
-    else
+
+    if (!busy && ir_event)
     {
-        if (waiting_msg)
-        {
-            lcd_screen.clear();
-            lcd_screen.display_message("Dog Feeder", 0, 0);
-            lcd_screen.display_message("Waiting for dog", 1, 0);
-            waiting_msg = false;
-        }
+        noInterrupts();
+        ir_event = false;
+        interrupts();
+
+        busy = true;
+        waiting_msg = false;
+
+        handleDogDetected();
+
+        busy = false;
     }
-    previous_state = current_state;
+
+    yield();
 }
